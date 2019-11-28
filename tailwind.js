e0253957e8f2bf26eb89fbfaa7b7183a0dc07524
Pedro Ladeira <pedr.ladeira@gmail.com>
2019-11-28 16:58:05 +0000

    add new colors

--- tailwind.js
+++ tailwind.js
@@ -2,9 +2,15 @@ module.exports = {
     theme: {
         extend: {
             colors: {
+                gnblack: '#040507',
+                gngray: '#A0A1B3',
+                'gngray-light': '#EAECF4',
+                'gngray-dark': '#5D5C62',
+                gnblue: '#3638F3',
+                'gnblue-light': '#508ED3',
             },
         }
     },
     variants: {},
     plugins: []
-}
+};
